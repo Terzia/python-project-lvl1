5fb9c25e402996545da3f1d6efe4136819324280
Elena Shlius <eur1ezs@gmail.com>
2022-05-24 17:59:23 +0300

    step 5 - add brain_even game

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from brain_games.game_logic import guess_even
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    guess_even()
+
+
+if __name__ == '__main__':
+    main()
