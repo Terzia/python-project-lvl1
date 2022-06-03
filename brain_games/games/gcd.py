da89b80ab70d14d90563f0b880d44069fdca6398
Elena Shlius <eur1ezs@gmail.com>
2022-06-03 16:30:23 +0300

    'correct question in gcd.py'

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -10,7 +10,7 @@ def question():
 def game():
     number1 = random.randint(1, 100)
     number2 = random.randint(1, 100)
-    print(f'Question: {number1} and {number2}')
+    print(f'Question: {number1} {number2}')
     ans = prompt.string('Your answer: ')
     correct_ans = math.gcd(number1, number2)
     return [int(ans), correct_ans]
